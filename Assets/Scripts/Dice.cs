ffcb44b80ee43ba297775f2dca10c07206b6acea
Iris Pecquet <iris.pecquet@lacatholille.fr>
2022-07-16 00:07:08 +0200

    Get highest dice face

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,3 +1,5 @@
+using RSLib.EditorUtilities;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +7,26 @@ using UnityEngine;
 public class Dice : MonoBehaviour
 {
     [SerializeField]
-    private DiceFace[] _diceFaces;
+    private DiceFace[] _diceFaces = null;
+
+    private DiceFace highestFace;
+
+    [ContextMenu("Get Highest Face")]
+    private DiceFace GetHighestFace()
+    {
+        float highestPos = float.MinValue;
+        
+        for (int i = 0; i < _diceFaces.Length; i++)
+        {
+            float posY = _diceFaces[i].transform.position.y;
+
+            if (posY > highestPos)
+            {
+                highestPos = posY;
+                highestFace = _diceFaces[i];
+            }
+        }
+
+        return highestFace;
+    }
 }
