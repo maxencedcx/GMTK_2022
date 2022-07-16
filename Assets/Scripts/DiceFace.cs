1cb3df5baa41c6e3ca13bd1d839fcdaf3674de5a
Robin Six <six.robin@hotmail.fr>
2022-07-16 22:47:41 +0200

    Dice faces symbols setup.

--- Assets/Scripts/DiceFace.cs
+++ Assets/Scripts/DiceFace.cs
@@ -1,17 +1,41 @@
+using System.Linq;
 using UnityEngine;
 
 public class DiceFace : MonoBehaviour
 {
+    [System.Serializable]
+    public struct DiceEffectMaterial
+    {
+        public DiceEffectType EffectType;
+        public Material Material;
+    }
+    
     [SerializeField]
     private MeshRenderer _meshRenderer = null;
 
     [SerializeField]
     private Material _material = null;
 
+    [SerializeField]
+    private DiceEffectMaterial[] _diceEffectMaterials = null;
+    
+    [SerializeField]
+    private MeshRenderer _effectTypeMaterial = null;
+    
     public DiceEffectType EffectType { get; private set; }
     
     public void SetEffectType(DiceEffectType diceEffectType)
     {
         EffectType = diceEffectType;
+
+        if (diceEffectType == DiceEffectType.NONE)
+        {
+            this._effectTypeMaterial.gameObject.SetActive(false);
+        }
+        else
+        {
+            this._effectTypeMaterial.material = _diceEffectMaterials.FirstOrDefault(o => o.EffectType == diceEffectType).Material;
+            this._effectTypeMaterial.gameObject.SetActive(true);
+        }
     }
 }
