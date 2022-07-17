343ff187301c1eb5816a216725ec22667809a35e
Iris Pecquet <iris.pecquet@lacatholille.fr>
2022-07-17 14:17:28 +0200

    Timer and score feedbacks

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -1,6 +1,8 @@
 using RSLib.Data;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
+using System;
 
 public class ScoreView : MonoBehaviour
 {
@@ -10,6 +12,14 @@ public class ScoreView : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _scoreText;
 
+    [SerializeField, Range(0.1f, 3)]
+    private float _punchIntensity;
+
+    [SerializeField, Range(0.1f, 3)]
+    private float _punchDuration;
+
+    private DG.Tweening.Tween _punchEffect;
+
     private void Awake()
     {
         this._scoreInt.ValueChanged += this.UpdateScoreText;
@@ -18,5 +28,12 @@ public class ScoreView : MonoBehaviour
     private void UpdateScoreText(Int.ValueChangedEventArgs args)
     {
         this._scoreText.text = args.New.ToString();
+        OnScoreUpdate();
+    }
+
+    private void OnScoreUpdate()
+    {
+        this._punchEffect?.Kill();
+        this._punchEffect = this._scoreText.transform.DOPunchScale(Vector3.one * this._punchIntensity, this._punchDuration, 0, 0f);
     }
 }
