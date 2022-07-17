cb07948f6251a9f00d7daf861d215ae77a7f5fd3
Maxence <maxence.delacroix@epitech.eu>
2022-07-17 17:27:04 +0200

    goal animations

--- Assets/Scripts/FollowPosition.cs
+++ Assets/Scripts/FollowPosition.cs
@@ -4,9 +4,12 @@ public class FollowPosition : MonoBehaviour
 {
     [SerializeField]
     private Transform _target = null;
+    
+    [SerializeField]
+    private Vector3 offset = Vector3.zero;
 
     private void Update()
     {
-        this.transform.position = this._target.position;
+        this.transform.position = this._target.position + this.offset;
     }
 }
